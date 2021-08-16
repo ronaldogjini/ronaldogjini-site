c060119c21c4d89ac18b442277a30d2f9a0d1893
ronaldogjini <ronaldogjini@users.noreply.github.com>
2021-08-16 19:45:33 +0200

    Revamped portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -51,148 +51,113 @@ function Portfolio() {
                 title="Ronaldo Gjini | Portfolio"
                 description="Software Projects developed by Ronaldo Gjini."
             />
-            <div className="max-w-full ">
-                <div className="h-48 bg-black flex justify-center items-center">
-                    <h1 className="max-w-screen-sm text-6xl text-white font-bold mb-4">Portfolio</h1>
+            <div className="max-w-full">
+                <div className="flex flex-col justify-center h-40 max-w-screen-xl px-4 mx-auto">
+                    <h1 className="max-w-screen-sm text-6xl font-bold mb-4">Recent Work</h1>
+                    <p className="">WEB • MOBILE • UX • BRANDING </p>
                 </div>
 
-                <div className=" py-8 z-10">
-                    <div className="max-w-screen-xl rounded-xl shadow-md mx-auto overflow-hidden">
-                        <div className="lg:flex justify-between items-center">
-                            <div className='p-8 space-y-4'>
-                                <h2 className="text-5xl  tracking-wide text-black font-bold">NaldoTech</h2>
-                                <p className="text-gray-500 text-2xl">Tech site focused on the latest news and guides about technology.</p>
-                                <div className=" text-blue-500 font-semibold">
-                                    <a className="flex items-center text-lg" target="_blank" rel="nofollow noopener noreferrer" href="https://www.naldotech.com">Visit site <ArrowRightIcon className=" ml-2 h-6 w-6 text-blue-500" /></a>
-                                </div>
-                            </div>
-                            <div class="md:flex-shrink-0">
-                                <StaticImage
-                                    className="w-full md:h-full"
-                                    src="../images/projects/naldo.png"
-                                    alt="NaldoTech"
-                                    width={550}
-                                />
+
+                <div className="md:flex max-w-screen-xl mx-auto pt-8">
+                    <div className="px-4 pb-16 md:w-1/2">
+                        <StaticImage
+                            className="w-full"
+                            src="../images/projects/naldo.png"
+                            alt="NaldoTech"
+                            width={1000}
+                        />
+                        <div className="pt-8">
+                            <h2 className="text-4xl  tracking-wide text-black font-bold">NaldoTech</h2>
+                            <p className="text-gray-500 text-xl py-4">Tech site focused on the latest news and guides about technology.</p>
+                            <div className=" text-blue-500 font-semibold">
+                                <a className="flex items-center text-lg" target="_blank" rel="nofollow noopener noreferrer" href="https://www.naldotech.com">Visit site</a>
                             </div>
                         </div>
                     </div>
-                </div >
-
-                <div className=" py-8 z-10">
-                    <div className="max-w-screen-xl rounded-xl shadow-md mx-auto overflow-hidden">
-                        <div className="lg:flex justify-between items-center">
-                            <div className='p-8 space-y-4'>
-                                <h2 className="text-5xl  tracking-wide text-black font-bold">Point Hotel Management</h2>
-                                <p className="text-gray-500 text-2xl">A fully featured hotel management suite with a custom booking engine, room management, and houskeeping panel.</p>
-                                <div className=" text-blue-500 font-semibold">
-                                    <a className="flex items-center text-lg" target="_blank" rel="nofollow noopener noreferrer" href="https://master.ddhjc7cf0obl7.amplifyapp.com/">Visit site <ArrowRightIcon className=" ml-2 h-6 w-6 text-blue-500" /></a>
-                                </div>
-                            </div>
-                            <div class="md:flex-shrink-0">
-                                <StaticImage
-                                    className="w-full md:h-full"
-                                    src="../images/projects/point.webp"
-                                    alt="Point Hotel Management"
-                                    width={550}
-                                />
+                    <div className="px-4 pb-16 md:w-1/2">
+                        <StaticImage
+                            className="w-full"
+                            src="../images/projects/nlp.webp"
+                            alt="NLP Language Identifier"
+                            width={1000}
+                        />
+                        <div className='pt-8'>
+                            <h2 className="text-4xl  tracking-wide text-black font-bold">NLP Language Identifier</h2>
+                            <p className="text-gray-500 text-xl py-4">A complete language identifier using the N-GRAM model.</p>
+                            <div className=" text-blue-500 font-semibold">
+                                <a className="flex items-center text-lg" target="_blank" rel="nofollow noopener noreferrer" href="https://www.whatlanguageisthistext.com/">Visit site</a>
                             </div>
                         </div>
                     </div>
-                </div >
+                </div>
 
-                <div className=" py-8 z-10">
-                    <div className="max-w-screen-xl rounded-xl shadow-md mx-auto overflow-hidden">
-                        <div className="lg:flex justify-between items-center">
-                            <div className='p-8 space-y-4'>
-                                <h2 className="text-5xl  tracking-wide text-black font-bold">NLP Language Identifier</h2>
-                                <p className="text-gray-500 text-2xl">A complete language identifier using the N-GRAM model.</p>
-                                <div className=" text-blue-500 font-semibold">
-                                    <a className="flex items-center text-lg" target="_blank" rel="nofollow noopener noreferrer" href="https://www.whatlanguageisthistext.com/">Visit site <ArrowRightIcon className=" ml-2 h-6 w-6 text-blue-500" /></a>
-                                </div>
-                            </div>
-                            <div class="md:flex-shrink-0">
-                                <StaticImage
-                                    className="w-full md:h-full"
-                                    src="../images/projects/nlp.webp"
-                                    alt="NLP Language Identifier"
-                                    width={550}
-                                />
+                <div className="md:flex max-w-screen-xl mx-auto">
+                    <div className="px-4 pb-16 md:w-1/2">
+                        <StaticImage
+                            className="w-full"
+                            src="../images/projects/point.webp"
+                            alt="Point Hotel Management"
+                            width={1000}
+                        />
+                        <div className='pt-8'>
+                            <h2 className="text-4xl  tracking-wide text-black font-bold">Point Hotel Management</h2>
+                            <p className="text-gray-500 text-xl py-4">A fully featured hotel management suite with a custom booking engine, room management, and houskeeping panel.</p>
+                            <div className=" text-blue-500 font-semibold">
+                                <a className="flex items-center text-lg" target="_blank" rel="nofollow noopener noreferrer" href="https://master.ddhjc7cf0obl7.amplifyapp.com/">Visit site</a>
                             </div>
                         </div>
                     </div>
-                </div >
+                    <div className="px-4 pb-16 md:w-1/2">
+                        <StaticImage
+                            className="w-full"
+                            src="../images/projects/student.webp"
+                            alt="Student Center Mobile App"
+                            width={1000}
+                        />
+                        <div className='pt-8'>
+                            <h2 className="text-4xl tracking-wide text-black font-bold">Student Center</h2>
+                            <p className="text-gray-500 text-xl py-4">Mobile app keeping track of students' performance.</p>
+                        </div>
+                    </div>
+                </div>
 
-
-                <div className=" py-8 z-10">
-                    <div className="max-w-screen-xl rounded-xl shadow-md mx-auto overflow-hidden">
-                        <div className="lg:flex justify-between items-center">
-                            <div className='p-8 space-y-4'>
-                                <h2 className="text-5xl  tracking-wide text-black font-bold">Bear PawBakery</h2>
-                                <p className="text-gray-500 text-2xl">The first creamer company in the United Kingdom with the highest quality.</p>
-                                <div className=" text-blue-500 font-semibold">
-                                    <a className="flex items-center text-lg" target="_blank" rel="nofollow noopener noreferrer" href="https://www.bearpawbakery.com/">Visit site <ArrowRightIcon className=" ml-2 h-6 w-6 text-blue-500" /></a>
-                                </div>
-                            </div>
-                            <div class="md:flex-shrink-0 ">
-                                <StaticImage
-                                    className="w-full md:h-full"
-                                    src="../images/projects/bearpawbakery.webp"
-                                    alt="BearPawBakery"
-                                    width={550}
-                                />
+                <div className="md:flex max-w-screen-xl mx-auto">
+                    <div className="px-4 pb-16 md:w-1/2">
+                        <StaticImage
+                            className="w-full"
+                            src="../images/projects/bearpawbakery.webp"
+                            alt="Bear Paw Bakery"
+                            width={1000}
+                        />
+                        <div className='pt-8'>
+                            <h2 className="text-4xl  tracking-wide text-black font-bold">Bear PawBakery</h2>
+                            <p className="text-gray-500 text-xl py-4">The first creamer company in the United Kingdom with the highest quality.</p>
+                            <div className=" text-blue-500 font-semibold">
+                                <a className="flex items-center text-lg" target="_blank" rel="nofollow noopener noreferrer" href="https://www.bearpawbakery.com/">Visit site</a>
                             </div>
                         </div>
                     </div>
-                </div >
-
-                <div className=" py-8 z-10">
-                    <div className="max-w-screen-xl rounded-xl shadow-md mx-auto overflow-hidden">
-                        <div className="lg:flex justify-between items-center">
-                            <div className='p-8 space-y-4'>
-                                <h2 className="text-5xl  tracking-wide text-black font-bold">LifeMagazine</h2>
-                                <p className="text-gray-500 text-2xl">A lifestyle magazine for everyone.</p>
-                                <div className=" text-blue-500 font-semibold">
-                                    <a className="flex items-center text-lg" target="_blank" rel="nofollow noopener noreferrer" href="https://lifemagazine.al">Visit site <ArrowRightIcon className=" ml-2 h-6 w-6 text-blue-500" /></a>
-                                </div>
-                            </div>
-                            <div class="md:flex-shrink-0 ">
-                                <StaticImage
-                                    className="w-full md:h-full"
-                                    src="../images/projects/lifemagazine.webp"
-                                    alt="Lifemagazine.al"
-                                    width={550}
-                                />
+                    <div className="px-4 pb-16 md:w-1/2">
+                        <StaticImage
+                            className="w-full"
+                            src="../images/projects/lifemagazine.webp"
+                            alt="Life Magazine Albania"
+                            width={1000}
+                        />
+                        <div className='pt-8'>
+                            <h2 className="text-4xl  tracking-wide text-black font-bold">LifeMagazine</h2>
+                            <p className="text-gray-500 text-xl py-4">A lifestyle magazine for everyone.</p>
+                            <div className=" text-blue-500 font-semibold">
+                                <a className="flex items-center text-lg" target="_blank" rel="nofollow noopener noreferrer" href="https://lifemagazine.al/">Visit site</a>
                             </div>
                         </div>
                     </div>
-                </div >
+                </div>
 
-                <div className=" py-8 z-10">
-                    <div className="max-w-screen-xl rounded-xl shadow-md mx-auto overflow-hidden">
-                        <div className="lg:flex justify-between items-center">
-                            <div className='p-8 space-y-4'>
-                                <h2 className="text-5xl  tracking-wide text-black font-bold">Chunkzone</h2>
-                                <p className="text-gray-500 text-2xl">Chunkzone is an online technology platform that provides downloading services</p>
-                                <div className=" text-blue-500 font-semibold">
-                                    <a className="flex items-center text-lg" target="_blank" rel="nofollow noopener noreferrer" href="https://chunkzone.com">Visit site <ArrowRightIcon className=" ml-2 h-6 w-6 text-blue-500" /></a>
-                                </div>
-                            </div>
-                            <div class="md:flex-shrink-0 ">
-                                <StaticImage
-                                    className="w-full md:h-full"
-                                    src="../images/projects/chunkzone.webp"
-                                    alt="Chunkzone"
-                                    width={550}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div >
-
-                {/* <div className="">
-                    {companies.map((company) => <PortfolioWork company={company}></PortfolioWork>)}
-                </div> */}
             </div>
+
+
+
         </Layout>
     )
 }
